f940e474fc6b6734da442a041f4195b1d2babdf9
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-07-05 10:24:50 -0500

    feat: :rocket: inicio

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>HOLA MUNDO</h1>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<script></script>
 
 <style lang="scss">
 #app {
